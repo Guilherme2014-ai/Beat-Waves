a09f27ef5c58a423cda9e85248955962968fe5b9
Guilherme2014-ai <guizuh_2014@outlook.com>
2022-11-03 23:44:09 -0300

    Updates

--- pages/searchResults/searchReults.js
+++ pages/searchResults/searchReults.js
@@ -4,7 +4,6 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 const search = params.search;
-
 const musics = Music.SearchMusics(search);
 
 const musicContainerElement = document.getElementById("musics__container");
