74f137d872abed5b4eecb1e32a0139ca850d8f04
Guilherme2014-ai <guizuh_2014@outlook.com>
2022-11-06 23:13:33 -0300

    player improved

--- src/usecases/setNewMusic.js
+++ src/usecases/setNewMusic.js
@@ -2,10 +2,16 @@ import { playerAudio } from "../index.js";
 
 export async function setNewMusic(e, musicSlug, musicName, musicOwner, musicUrl) {
     e.stopPropagation();
+
+    if (!playerAudio._isActive) {
+        playerAudio.writePlayer();
+        playerAudio.setActive();
+    }
     
     playerAudio.remove();
     playerAudio.setSrc(musicSlug);
-
+    
+    
     playerAudio.setPlayingNow(musicName);
     playerAudio.setMusicOwner(musicOwner);
     playerAudio.setMusicProfilePicture(musicUrl);
